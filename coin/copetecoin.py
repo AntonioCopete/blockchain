61ccdc3900420e0e5dd52ba436b28f70c0b5a8ac
AntonioCopete <antcopjim@gmail.com>
2022-01-04 11:57:07 +0100

    feat transactions, nodes and largest chain replace

--- coin/copetecoin.py
+++ coin/copetecoin.py
@@ -14,19 +14,24 @@ from flask import Flask, jsonify, request
 import requests
 from uuid import uuid4
 from urllib import urlparse
+
 # Part 1 - Create the blockchain
 class Blockchain:
     
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
         
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain)+1,
                  "timestamp": str(datetime.datetime.now()),
                  "proof": proof,
-                 "previous_hash": previous_hash
+                 "previous_hash": previous_hash,
+                 "transactions": self.transactions
             }
+        self.transactions = []
         self.chain.append(block)
         return block
     
@@ -63,6 +68,35 @@ class Blockchain:
             previous_block = block
             block_index +=1
         return True
+    
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender':sender,
+                                  'receiver': receiver,
+                                  'amount': amount})
+        previous_block = self.previous_block()
+        return previous_block['index'] + 1    
+    
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+        
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code = 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+                    
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
     
 # Part 2 - Blockchain mining
 
